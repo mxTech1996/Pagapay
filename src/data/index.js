23b6a58efe450dd606a6f0c2b93e295fcfb5fb0c
juliotwo <juliotwo1996@gmail.com>
2025-05-09 23:49:21 -0600

    fixe

--- src/data/index.js
+++ src/data/index.js
@@ -40,3 +40,255 @@ export const footerData = [
     label: 'Terms',
   },
 ];
+
+export const dataSite = {
+  name: 'Pagapay',
+  url: 'https://www.azucarzaz.lat/',
+  telephone: '5523022173',
+  email: 'admin@pagapay.mx',
+  address:
+    'REAL DE MINAS MANZANA 2 LOTE 2, CASA 10, SM 45 HDA VALLARTA CANCUN, BENITO JUAREZ, CANCUN, C.P. 77506',
+  description:
+    'Our  provides fast, secure, and seamless payment processing solutions for businesses of all sizes. From online checkouts to in-store transactions, we enable  to accept payments via credit cards, digital wallets, and bank transfers with ease.',
+  subtitle:
+    'Secure, Scalable Payment Solutions for a Seamless Checkout Experience',
+  image_hero:
+    'https://cms-webserver-statics.s3.amazonaws.com/media/websites/paga_1.jpg',
+  image_hero2:
+    'https://cms-webserver-statics.s3.amazonaws.com/media/websites/paga._2.jpg',
+  iconImage:
+    'https://cms-webserver-statics.s3.amazonaws.com/media/websites/pagapay-high-resolution-logo.png',
+  info: [
+    {
+      title: 'Innovative in the market',
+      description:
+        'We are pioneers in payment processing, redefining how businesses and consumers exchange value in the digital age. Through cutting edge technology, intelligent automation, and a customer-first approach, we deliver faster, safer, and more flexible payment solutions. From real time transactions to advanced fraud prevention, our innovations are designed to streamline operations, boost conversion rates, and power the future of commerce.',
+    },
+    {
+      title: 'Payment processing system',
+      description:
+        'Our payment processing system enables businesses to accept and manage digital transactions with speed, security, and efficiency. Designed to support a wide range of payment methods including credit cards, debit cards, digital wallets, and bank transfers our platform ensures seamless integration with online stores, mobile apps, and point-of-sale systems.',
+    },
+    {
+      title: 'Security',
+      description:
+        'All our products have the highest encryption standards and security tests.',
+    },
+    {
+      title: 'Efficiency',
+      description:
+        'Day after day, our team of technicians strive to provide maintenance and react to unforeseen situations to mitigate the greatest number of errors.',
+    },
+    {
+      title: 'Leaders of change',
+      description:
+        'We believe in opportunities for all with more competitive businesses that generate growth and well-being for people.',
+    },
+    {
+      title: 'Accessibility in your payments',
+      description: 'All the tools are easy and simple to use',
+    },
+  ],
+  services: [
+    {
+      title: 'POS ,Integration',
+      description:
+        'Seamless connection with in-store systems to accept payments via terminals, QR codes, or contactless methods.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/pay_7_.jpg',
+    },
+    {
+      title: 'Online Payment Gateway',
+      description:
+        'Secure and fast processing of credit cards, debit cards, and digital wallets for e-commerce platforms.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/pay_5_.jpg',
+    },
+    {
+      title: 'Recurring Billing & Subscriptions',
+      description:
+        'Automated tools to manage recurring payments for subscription-based businesses.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/pay_6_.jpg',
+    },
+    {
+      title: 'Fraud Detection & Security Tools',
+      description:
+        'Real-time monitoring and advanced encryption to protect transactions and reduce chargebacks.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/pay_8_.jpg',
+    },
+    {
+      title: 'Multi Currency & Cross Border Payments',
+      description:
+        'Support for global transactions, allowing businesses to accept payments in various currencies with automatic conversion.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/pay_9_.jpg',
+    },
+  ],
+  references: [
+    {
+      name: 'Amina Homles, SaaS Product Founder',
+      description:
+        'We started with basic features and upgraded as our business expanded. The platform scaled with us and offered the flexibility we needed.',
+      rating: 5,
+      image: null,
+    },
+    {
+      name: 'Sofia Rua, Local Boutique Owner',
+      description:
+        'As a small business, finding a cost-effective and easy to integrate payment solution was key. This service checked all the boxes and helped us grow faster.',
+      rating: 5,
+      image: null,
+    },
+    {
+      name: 'James Lemnes, Digital Services Provide',
+      description:
+        "The analytics dashboard has helped us better understand our customers' behavior and optimize our checkout process",
+      rating: 5,
+      image: null,
+    },
+    {
+      name: 'Laura Mendez, E commerce Store Owner',
+      description:
+        "We've been using this payment platform for over a year, and the experience has been flawless. Transactions are fast, and the dashboard is very intuitive",
+      rating: 5,
+      image: null,
+    },
+    {
+      name: 'David Kingres, Subscription Business Manager',
+      description:
+        'What really sets them apart is their customer service. Anytime we had a question, the support team responded immediately. We also feel confident knowing our payments are secure',
+      rating: 5,
+      image: null,
+    },
+  ],
+  products: [
+    {
+      id: 690,
+      name: 'Pagapay terminal',
+      category: null,
+      description: 'An easy way to accept chip cards and contactless payments',
+      price: '599.99',
+      stock: 100,
+      content:
+        'Multiple Payment Method Support,Accepts credit and debit cards using EMV chip, magnetic stripe, and contactless ,NFC, technologies, including Apple Pay and Google Pay.,Versatile Connectivity\r\nConnects via Wi Fi, mobile data  or Bluetooth, making it ideal for both fixed points of sale and mobile use,Portable and Self Contained Design,Compact devices with long battery life perfect for businesses on the go, such as delivery services or vendors at markets and events,Integration with Management Apps,Compatible with mobile apps or POS systems to help manage inventory, track sales, and generate real time reports.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/pay_11.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 695,
+      name: 'Pagapay App',
+      category: null,
+      description:
+        'The Pagapay App is a mobile payment platform that allows users to transfer money electronically and make payments through their mobile devices.',
+      price: '899.99',
+      stock: 100,
+      content:
+        'Instant Money Transfers,Send money instantly to anyone with a phone number, email address, or bank account,Bill Payments,Pay for services such as Pay TV, internet, electricity, and more directly through the app.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/pay_16.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 691,
+      name: 'Light terminal',
+      category: null,
+      description: 'A simple way to accept chip cards and contactless payments',
+      price: '799.99',
+      stock: 100,
+      content:
+        'NFC , Technology,Faster Checkout Process,EMV and Magnetic Stripe Support,Secure Transactions,Mobile Wallet Compatibility,Compact and Wireless Design,Multi Device Pairing',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/pay_10_U7hRshB.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 692,
+      name: 'Remote payments',
+      category: null,
+      description:
+        'Our Remote Payments solution enables businesses to accept payments from customers anywhere—without needing a physical card terminal. It’s perfect for online sales, phone orders, home deliveries, or service-based businesses that operate remotely.',
+      price: '699.99',
+      stock: 100,
+      content:
+        'Payment Links & Invoices,QR Code Payments,,Mobile Wallet Support,Custom Branding,Real-Time Notifications,Multi-Currency & Global Support,Security & Compliance.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/pay_13.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 693,
+      name: 'Payment integrations',
+      category: null,
+      description: 'You can make online sales quickly and safely.',
+      price: '599.99',
+      stock: 100,
+      content:
+        "Our Payment Integrations solution allows businesses to seamlessly connect their existing platformswebsites, mobile apps, or point of sale systems with our secure payment gateway. It's designed for flexibility, speed, and scalability, enabling smooth and consistent transaction experiences across all customer touchpoints.",
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/pay_14.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 694,
+      name: 'QR Code Payment',
+      category: null,
+      description:
+        'Our QR Code Payment solution offers a fast, secure, and contactless way for customers to make payments by simply scanning a QR code with their smartphone. Ideal for physical stores, delivery services, and remote sales, this solution reduces friction at checkout and enhances customer convenience no terminal or card swipe required.',
+      price: '799.99',
+      stock: 100,
+      content:
+        'Instant QR Code Generation,Generate dynamic or static QR codes instantly for any transaction, invoice, or product,Customer Friendly Checkout,Customers scan the code using their preferred banking app or digital wallet,no need for physical cards or hardware.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/pay._15.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 696,
+      name: 'Payment Analytics Dashboard',
+      category: null,
+      description:
+        'Real time tracking of transactions, refunds, settlements, and insights into customer behavior.',
+      price: '899.99',
+      stock: 100,
+      content:
+        'The Payment Analytics Dashboard is a powerful reporting tool that gives businesses real-time visibility into their transaction data. It helps merchants monitor sales performance, track payment trends, and make informed financial decisions all from one centralized, easy to use interface.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/pay_15_.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 697,
+      name: 'Recurring Billing & Subscription Manager',
+      category: null,
+      description:
+        'The Recurring Billing & Subscription Manager is a comprehensive solution designed to automate and manage ongoing customer payments. Ideal for subscription-based businesses, SaaS platforms, membership services, and utility providers, it streamlines billing cycles, improves retention, and ensures consistent cash flow.',
+      price: '999.99',
+      stock: 100,
+      content:
+        'Automated Recurring Payments,Automatically charge customers on a set schedule weekly, monthly, annually, or custom intervals,Flexible Plan Creation Create and manage multiple pricing plans with options for free trials, promotional discounts, and usage based billing.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/pay_17.jpg',
+      image2: null,
+      image3: null,
+    },
+  ],
+  categories: [],
+};
