cbd26d2b54e6423e58e3fcf5d14c02cf2398b58f
juliotwo <juliotwo1996@gmail.com>
2025-07-02 17:48:39 -0600

    fix

--- src/components/organisms/Products.js
+++ src/components/organisms/Products.js
@@ -6,7 +6,7 @@ const terminalOptions = [
   {
     id: 2000,
     name: 'Mom V240',
-    image: '/images/product1.png',
+    image: '/images/product1.jpeg',
     features: {
       MagneticStripe: true,
       ChipCard: true,
@@ -20,14 +20,13 @@ const terminalOptions = [
       Dimensions: '60 x 39 x 14.2 mm',
       ChargingPort: 'Micro USB',
       Shipping: 'Free',
-      Fee: '3.5% + VAT',
       Available: true,
     },
   },
   {
     id: 2001,
     name: 'QPOS Mini',
-    image: '/images/product2.png',
+    image: '/images/product2.jpeg',
     features: {
       MagneticStripe: true,
       ChipCard: true,
@@ -41,7 +40,27 @@ const terminalOptions = [
       Dimensions: '99 x 59 x 15 mm',
       ChargingPort: 'Micro USB',
       Shipping: 'Free',
-      Fee: '3.5% + VAT',
+
+      Available: true,
+    },
+  },
+  {
+    id: 2002,
+    name: 'Android Terminal',
+    image: '/images/product3.jpeg',
+    features: {
+      MagneticStripe: true,
+      ChipCard: true,
+      Contactless: true,
+      RequiresPhone: true,
+      Screen: 'Monochrome (128x40)',
+      Camera: 'No',
+      MobileNetworks: 'Supported',
+      Display: 'Monochrome Display',
+      Weight: '80g',
+      Dimensions: '99 x 59 x 15 mm',
+      ChargingPort: 'Micro USB',
+      Shipping: 'Free',
       Available: true,
     },
   },
@@ -82,7 +101,10 @@ export default function ProductsComparison() {
                 <li>Commission: {terminal.features.Fee}</li>
               </ul>
               {terminal.features.Available ? (
-                <button className='bg-[#1A2F50] text-white flex items-center gap-2 px-4 py-2 rounded hover:bg-blue-700'>
+                <button
+                  onClick={() => (window.location.href = '/more-information')}
+                  className='bg-[#1A2F50] text-white flex items-center gap-2 px-4 py-2 rounded hover:bg-blue-700'
+                >
                   Buy Now <FaShoppingCart />
                 </button>
               ) : (
